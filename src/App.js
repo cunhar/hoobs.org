07ddd87551575d8a977daa28d2003fef15a6c1cb
Ricardo Cunha <cunhar@gmail.com>
2018-12-22 19:36:20 -0500

    Work on app layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
@@ -7,19 +7,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <div className="App-status">
+            <div className="App-live">LIVE</div>
+          </div>
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
+        <div className="App-body">
+
+        </div>
       </div>
     );
   }
